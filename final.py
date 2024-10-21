9aae79fd179951bc4ca36724a5cd68e26de0597d
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-10-21 20:41:33 +0000

    final

--- final.py
+++ final.py
@@ -1,1 +1,14 @@
+# Tic Tac Toe - Arsh, Nathaly, ,
 
+
+#We need our other teamates to start working
+
+#Board - 
+print("1 2 3\n4 5 6\n7 8 9")
+#Player input -
+input("Which slot do you want to select (1 - 9)")
+#Computer input (Random 1-8) - 
+
+#Keep going until 3 in a row - 
+
+#Check who wins - 
