b575d866eecbccf5be494c9e625fac6d4afe0742
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-10-22 20:42:09 +0000

    tic tac toe 1

--- final.py
+++ final.py
@@ -1,14 +1,38 @@
-# Tic Tac Toe - Arsh, Nathaly, ,
+# Tic Tac Toe - Arsh, Nathaly, Isaac
 
 
-#We need our other teamates to start working
 
-#Board - 
-print("1 2 3\n4 5 6\n7 8 9")
-#Player input -
-input("Which slot do you want to select (1 - 9)")
-#Computer input (Random 1-8) - 
+#Board - Group
+board = print("1 2 3\n4 5 6\n7 8 9")
 
-#Keep going until 3 in a row - 
+#Player 1 input - Arsh
+X = input("Where would you like to go?")
+if X == 1:
+    board = print("X 2 3\n4 5 6\n7 8 9")
+elif X == 2:
+    board = print("1 X 3\n4 5 6\n7 8 9")
+elif X == 3:
+    board = print("1 2 X\n4 5 6\n7 8 9")
+elif X == 4:
+    board = print("1 2 3\nX 5 6\n7 8 9")
+elif X == 5:
+    board = print("1 2 3\n4 X 6\n7 8 9")
+elif X == 6:
+    board = print("1 2 3\n4 5 X\n7 8 9")
+elif X == 7:
+    board = print("1 2 3\n4 5 6\nX 8 9")
+elif X == 8:
+     board = print("1 2 3\n4 5 6\n7 X 9")
+elif X == 9:
+    board = print("1 2 3\n4 5 6\n7 8 X")
 
-#Check who wins - 
+
+
+#Player 2 input - Nathaly
+#O = input("Where would you like to go?")
+#if O is X:
+#    print("Already taken")
+
+#Keep going - I
+
+#Check who wins
