1050fc87a7e4e4df2ce2c2c237acc2943e14640b
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-10-23 20:42:19 +0000

    final o

--- final.py
+++ final.py
@@ -1,43 +1,71 @@
 # Tic Tac Toe - Arsh, Nathaly, Isaac
 
-
-
 #Board - Group
-board = "1 2 3\n4 5 6\n7 8 9"
-print(board)
+board1 = ["1", "2", "3"]
+board2 = ["4", "5", "6"]
+board3 = ["7", "8", "9"]
+print("", board1,"\n", board2,"\n", board3)
 
 #Player 1 input - Arsh
-
+#def player(playing):
+#     return(int(input(f"How much is your {}?: ")))
 X = int(input("Player 1's turn\nChoose a slot 1-9:\n"))
 if X == 1:
-    board = "X 2 3\n4 5 6\n7 8 9"
-elif board == 2:
-    board = "1 X 3\n4 5 6\n7 8 9"
+     board1[0]="X"
+elif X == 2:
+     board1[1]="X"
 elif X == 3:
-     board = "1 2 X\n4 5 6\n7 8 9"
+     board1[2]="X"
 elif X == 4:
-     board = "1 2 3\nX 5 6\n7 8 9"
+     board2[3]="X"
 elif X == 5:
-     board = "1 2 3\n4 X 6\n7 8 9"
+     board2[4]="X"
 elif X == 6:
-     board = "1 2 3\n4 5 X\n7 8 9"
+     board2[5]="X"
 elif X == 7:
-     board = "1 2 3\n4 5 6\nX 8 9"
+     board3[6]="X" 
 elif X == 8:
-     board = "1 2 3\n4 5 6\n7 X 9"
+     board3[7]="X"
 elif X == 9:
-    board = "1 2 3\n4 5 6\n7 8 X"
+     board3[8]="X"
 else:
     print("You cannot input that")
-print(board)
-
+print("", board1,"\n", board2,"\n", board3)
 
 
 #Player 2 input - Nathaly
-#O = input("Where would you like to go?")
-#if O is X:
-#    print("Already taken")
+O = int(input("Player 2's turn\nChoose a slot 1-9:\n"))
+if O == 1:
+     board1[0]="O"
+elif O == 2:
+     board1[1]="O"
+elif O == 3:
+     board1[2]="O"
+elif O == 4:
+     board2[3]="O"
+elif O == 5:
+     board2[4]="O"
+elif O == 6:
+     board2[5]="O"
+elif O == 7:
+     board3[6]="O" 
+elif O == 8:
+     board3[7]="O"
+elif O == 9:
+     board3[8]="O"
+else:
+    print("You cannot input that")
+print("", board1,"\n", board2,"\n", board3)
 
-#Keep going - I
+#O = input("Where wou
 
-#Check who wins
+if O is X:
+    print("Already taken.")
+
+#Keep going - Isaac
+
+
+
+
+
+#Check who wins - 
