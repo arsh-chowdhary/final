287ac7332776a5f9b66faf677d3df65ae196360e
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-10-24 19:43:23 +0000

    check who wins

--- final.py
+++ final.py
@@ -10,7 +10,7 @@ print("", board1,"\n", board2,"\n", board3)
 def playing(player):
      return(int(input(f"{player} turn\nChoose a slot 1-9 that has not been taken: ")))
 
-X = playing("Player 1's")
+X = playing("X's")
 if X == 1:
      board1[0]="X"
 elif X == 2:
@@ -18,17 +18,17 @@ elif X == 2:
 elif X == 3:
      board1[2]="X"
 elif X == 4:
-     board2[3]="X"
+     board2[0]="X"
 elif X == 5:
-     board2[4]="X"
+     board2[1]="X"
 elif X == 6:
-     board2[5]="X"
+     board2[2]="X"
 elif X == 7:
-     board3[6]="X" 
+     board3[0]="X" 
 elif X == 8:
-     board3[7]="X"
+     board3[1]="X"
 elif X == 9:
-     board3[8]="X"
+     board3[2]="X"
 else:
     print("You cannot input that")
 print("", board1,"\n", board2,"\n", board3)
@@ -38,10 +38,10 @@ print("", board1,"\n", board2,"\n", board3)
 def slot(number):
      return(int(input(f"{number} has already been taken!")))
 
-O = playing("Player 2's")
+O = playing("O's")
 if O==X:
      print(slot(X))
-     O = input("Player 2's turn\nChoose a slot 1-9 that has not been taken: ")
+     O = input("O's turn\nChoose a slot 1-9 that has not been taken: ")
 elif O == 1:
      board1[0]="O"
 elif O == 2:
@@ -49,25 +49,128 @@ elif O == 2:
 elif O == 3:
      board1[2]="O"
 elif O == 4:
-     board2[3]="O"
+     board2[0]="O"
 elif O == 5:
-     board2[4]="O"
+     board2[1]="O"
 elif O == 6:
-     board2[5]="O"
+     board2[2]="O"
 elif O == 7:
-     board3[6]="O" 
+     board3[0]="O" 
 elif O == 8:
-     board3[7]="O"
+     board3[1]="O"
 elif O == 9:
-     board3[8]="O"
+     board3[2]="O"
 else:
     print("You cannot input that")
 print("", board1,"\n", board2,"\n", board3)
 
 #Keep going - Isaac
+X = playing("X's")
+if X == 1:
+     board1[0]="X"
+elif X == 2:
+     board1[1]="X"
+elif X == 3:
+     board1[2]="X"
+elif X == 4:
+     board2[0]="X"
+elif X == 5:
+     board2[1]="X"
+elif X == 6:
+     board2[2]="X"
+elif X == 7:
+     board3[0]="X" 
+elif X == 8:
+     board3[1]="X"
+elif X == 9:
+     board3[2]="X"
+else:
+    print("You cannot input that")
+print("", board1,"\n", board2,"\n", board3)
+
+O = playing("O's")
+if O == 1:
+     board1[0]="O"
+elif O == 2:
+     board1[1]="O"
+elif O == 3:
+     board1[2]="O"
+elif O == 4:
+     board2[0]="O"
+elif O == 5:
+     board2[1]="O"
+elif O == 6:
+     board2[2]="O"
+elif O == 7:
+     board3[0]="O" 
+elif O == 8:
+     board3[1]="O"
+elif O == 9:
+     board3[2]="O"
+else:
+    print("You cannot input that")
+print("", board1,"\n", board2,"\n", board3)
+
+X = playing("X's")
+if X == 1:
+     board1[0]="X"
+elif X == 2:
+     board1[1]="X"
+elif X == 3:
+     board1[2]="X"
+elif X == 4:
+     board2[0]="X"
+elif X == 5:
+     board2[1]="X"
+elif X == 6:
+     board2[2]="X"
+elif X == 7:
+     board3[0]="X" 
+elif X == 8:
+     board3[1]="X"
+elif X == 9:
+     board3[2]="X"
+else:
+    print("You cannot input that")
+print("", board1,"\n", board2,"\n", board3)
+
+O = playing("O's")
+if O == 1:
+     board1[0]="O"
+elif O == 2:
+     board1[1]="O"
+elif O == 3:
+     board1[2]="O"
+elif O == 4:
+     board2[0]="O"
+elif O == 5:
+     board2[1]="O"
+elif O == 6:
+     board2[2]="O"
+elif O == 7:
+     board3[0]="O" 
+elif O == 8:
+     board3[1]="O"
+elif O == 9:
+     board3[2]="O"
+else:
+    print("You cannot input that")
+print("", board1,"\n", board2,"\n", board3)
 
 
+#Check who wins - Arsh
+if board1 == ["X", "X", "X"]:
+     print("X Wins!")
+elif board2 == ["X", "X", "X"]:
+     print("X Wins!")
+elif board3 == ["X", "X", "X"]:
+     print("X Wins!")
+elif board1 == ["O", "O", "O"]:
+     print("O Wins!")
+elif board2 == ["O", "O", "O"]:
+     print("O Wins!")
+elif board3 == ["O", "O", "O"]:
+     print("O Wins!")
+elif board1[0] == "X" and board2[0] == "X" and board3[0] == "X":
+     print ("X Wins!")
 
-
-
-#Check who wins - 
