a26767072057f9dac8bde99853f98b4a8a1047ad
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-10-25 20:41:52 +0000

    final changes

--- final_copy.py
+++ final_copy.py
@@ -11,9 +11,6 @@ print("", board1,"\n", board2,"\n", board3)
 def playing(player):
      return(int(input(f"{player} turn\nChoose a slot 1-9 that has not been taken: ")))
 
-def slot(number):
-     return(int(input(f"{number} has already been taken!")))
-
 #Conditionals
 X = playing("X's")
 while True:
@@ -37,61 +34,63 @@ while True:
           board3[2]="X"
      else:
           print("You cannot input that")
-     print("", board1,"\n", board2,"\n", board3)
+print("", board1,"\n", board2,"\n", board3)
 
 #Player 2 input - Nathaly
 #Function 2
+def slot(number):
+     return(int(input(f"{number} has already been taken!")))
 
 O = playing("O's")
-     if O==X:
-          print(slot(X))
-     elif O == 1:
-          board1[0]="O"
-     elif O == 2:
-          board1[1]="O"
-     elif O == 3:
-          board1[2]="O"
-     elif O == 4:
-          board2[0]="O"
-     elif O == 5:
-          board2[1]="O"
-     elif O == 6:
-          board2[2]="O"
-     elif O == 7:
-          board3[0]="O" 
-     elif O == 8:
-          board3[1]="O"
-     elif O == 9:
-          board3[2]="O"
-     else:
-          print("You cannot input that")
-     print("", board1,"\n", board2,"\n", board3)
+if O==X:
+     print(slot(X))
+elif O == 1:
+     board1[0]="O"
+elif O == 2:
+     board1[1]="O"
+elif O == 3:
+     board1[2]="O"
+elif O == 4:
+     board2[0]="O"
+elif O == 5:
+     board2[1]="O"
+elif O == 6:
+     board2[2]="O"
+elif O == 7:
+     board3[0]="O" 
+elif O == 8:
+     board3[1]="O"
+elif O == 9:
+     board3[2]="O"
+else:
+    print("You cannot input that")
+print("", board1,"\n", board2,"\n", board3)
 
 #Keep going - Isaac
-     X = playing("X's")
-     if X==O:
-          print(slot(X))
-     elif X == 1:
-          board1[0]="X"
-     elif X == 2:
-          board1[1]="X"
-     elif X == 3:
-          board1[2]="X"
-     elif X == 4:
-          board2[0]="X"
-     elif X == 5:
-          board2[1]="X"
-     elif X == 6:
-          board2[2]="X"
-     elif X == 7:
-          board3[0]="X" 
-     elif X == 8:
-          board3[1]="X"
-     elif X == 9:
-          board3[2]="X"
-     else:
-          print("You cannot input that")
-     print("", board1,"\n", board2,"\n", board3)
+X = playing("X's")
+if X==O:
+     print(slot(X))
+elif X == 1:
+     board1[0]="X"
+elif X == 2:
+     board1[1]="X"
+elif X == 3:
+     board1[2]="X"
+elif X == 4:
+     board2[0]="X"
+elif X == 5:
+     board2[1]="X"
+elif X == 6:
+     board2[2]="X"
+elif X == 7:
+     board3[0]="X" 
+elif X == 8:
+     board3[1]="X"
+elif X == 9:
+     board3[2]="X"
+else:
+    print("You cannot input that")
+print("", board1,"\n", board2,"\n", board3)
 
 O = playing("O's")
 if O==X:
